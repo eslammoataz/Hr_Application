8d7331edb881e1d45dda9ab693078de57a2977d8
eslamelmoataz <eslam.elmoataz@mtor.co>
2026-03-16 02:42:34 +0200

    feat: implement company creation command handler and AutoMapper profiles.

--- HrSystemApp.Application/Features/Companies/Commands/CreateCompany/CreateCompanyCommandHandler.cs
+++ HrSystemApp.Application/Features/Companies/Commands/CreateCompany/CreateCompanyCommandHandler.cs
@@ -1,40 +1,30 @@
+using AutoMapper;
 using MediatR;
 using HrSystemApp.Application.Common;
 using HrSystemApp.Application.DTOs.Companies;
 using HrSystemApp.Application.Interfaces;
 using HrSystemApp.Domain.Models;
-using HrSystemApp.Domain.Enums;
 
 namespace HrSystemApp.Application.Features.Companies.Commands.CreateCompany;
 
 public class CreateCompanyCommandHandler : IRequestHandler<CreateCompanyCommand, Result<CompanyResponse>>
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
 
-    public CreateCompanyCommandHandler(IUnitOfWork unitOfWork)
+    public CreateCompanyCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
     {
         _unitOfWork = unitOfWork;
+        _mapper = mapper;
     }
 
     public async Task<Result<CompanyResponse>> Handle(CreateCompanyCommand request, CancellationToken cancellationToken)
     {
-        var company = new Company
-        {
-            CompanyName = request.CompanyName,
-            CompanyLogoUrl = request.CompanyLogoUrl,
-            YearlyVacationDays = request.YearlyVacationDays,
-            Status = CompanyStatus.Active
-        };
+        var company = _mapper.Map<Company>(request);
 
         await _unitOfWork.Companies.AddAsync(company, cancellationToken);
         await _unitOfWork.SaveChangesAsync(cancellationToken);
 
-        return Result.Success(new CompanyResponse(
-            company.Id,
-            company.CompanyName,
-            company.CompanyLogoUrl,
-            company.YearlyVacationDays,
-            company.Status.ToString()
-        ));
+        return Result.Success(_mapper.Map<CompanyResponse>(company));
     }
 }
--- HrSystemApp.Application/Mappings/CompanyMappingProfile.cs
+++ HrSystemApp.Application/Mappings/CompanyMappingProfile.cs
@@ -1,5 +1,7 @@
 using AutoMapper;
 using HrSystemApp.Application.DTOs.Companies;
+using HrSystemApp.Application.Features.Companies.Commands.CreateCompany;
+using HrSystemApp.Domain.Enums;
 using HrSystemApp.Domain.Models;
 
 namespace HrSystemApp.Application.Mappings;
@@ -8,6 +10,9 @@ public class CompanyMappingProfile : Profile
 {
     public CompanyMappingProfile()
     {
+        CreateMap<CreateCompanyCommand, Company>()
+            .AfterMap((_, dest) => dest.Status = CompanyStatus.Active);
+
         CreateMap<Company, CompanyResponse>()
             .ForMember(d => d.Status, o => o.MapFrom(s => s.Status.ToString()));
 
